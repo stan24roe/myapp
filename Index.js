074868d4743f986bc927ce09013b1ed040b7b6a9
Stan Roe Rosales <stan24roe@gmail.com>
2016-12-16 14:21:04 +0800

    firt commit

---
+++ Index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+var path = require('path');
+
+app.use(express.static(path.join(__dirname,"public")));
+
+let port = Number(process.env.PORT || 5000);
+app.listen(port,()=>{
+	console.log("port" + port);
+});
